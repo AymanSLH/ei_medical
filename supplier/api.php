c7500f4a8bb98f6bd46c159aaf4d334c4cce8e02
AymanSLH <ayman171salah@gmail.com>
2018-05-15 01:38:35 +0200

    Added skeleton Api files

---
+++ supplier/api.php
@@ -0,0 +1,38 @@
+<?php
+
+header("Content-Type:application/json") ;
+require 'data.php' ;
+
+if (!empty($_GET['name']))
+    {
+        $name = $_GET['name'] ;
+        $price = get_price($name) ;
+
+        if (empty($price))
+            {
+                resposne(200 , "Product Not Found" , NULL) ;
+            }
+
+        else
+            {
+                response(200 , "Prosuct Found" , $price) ;
+            }
+    }
+
+else
+    {
+        response(400,"Invalid Request",NULL);
+    }
+
+
+function response($status,$status_message,$data)
+{
+    header("HTTP/1.1 ".$status);
+
+    $response['status']=$status;
+    $response['status_message']=$status_message;
+    $response['data']=$data;
+
+    $json_response = json_encode($response);
+    echo $json_response;
+}
